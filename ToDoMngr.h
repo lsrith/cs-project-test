efcf9ebadbd876cb8b278a2203d5d12e1a22e63c
lsrith <lsrith@gmail.com>
2011-09-17 17:42:04 +0700

    Update function prototype

--- ToDoMngr.h
+++ ToDoMngr.h
@@ -9,6 +9,7 @@
 using namespace std;
 
 enum searchType {EXACT, SIMILAR};
+enum repeatType {};
 
 class ToDoMngr {
 private:
@@ -19,7 +20,6 @@ private:
 	stack<string> _redoHistory;
 	string* _shortcut;
 	string* _command;
-	list<Template> _tasks;
 
 	void splitCommand ();
 	bool invalidCommand ();
@@ -33,9 +33,13 @@ private:
 	void CLdisplay ();
 
 //Ris:
+	string view (Template* task);
+	// return a string of the view of the specific task
 	string view (string viewType);
-	// return a string of the view of the specific task, by day, by week or by table name
+	// return a string of the view of the specific tasks, by day, by week or by table name
 	// have to consider how to display the clashed tasks
+	// for each task shown, numering them from 1 to the end and save as a vector of address of template
+	// if user enter view 5, we execute view (templateVect[4]); 
 	string reminder ();
 	// return a string of the view of the reminder whenever ToDoMngr is executed;
 	string alert ();
@@ -53,6 +57,15 @@ private:
 	list<Template> load (TimePeriod period);
 	// return a list of templates within a time period input :) get it from our storageFile
 
+	void save (string tableName, TimePeriod period, list<string> tasksName);
+	// save the timetable with the name and period so that you can load back a tasks
+	list<Template> load (string tableName);
+	// return a list of templates of the timetable
+	list<string> load_table_name ();
+	// return a list of names of existing timetable
+
+	void reIndexingTemplates ();
+	// when to do it is based on your decision @chew
 	string search (string searchedWord, searchType type);
 	// search based on any specified word.. return a string to be displayed
 	// type to indicate exact match or similar match
@@ -61,28 +74,34 @@ private:
 
 //Ben:
 	Template addTask (Template task, bool forceAdd);
-	//add a task to the dataStorage
-	//if forceAdd is true, just add and return a fresh Template
-	//else return the clashed task
+	// add a task to the dataStorage
+	// if forceAdd is true, just add and return a fresh Template
+	// else return the clashed task
+
+	list<Template> addTask (Template task, repeatType repeat, bool forceAdd);
+	// add a task to the dataStorage based on repeat type
 
 	void editTask (/*argument to be specified*/);
+	Template editTask (/*argument to be specified*/);
+	list<Template> editTask (/*argument to be specified*/);
 	void eraseTask (/*argument to be specified*/);
-	//functions overloading are considerable
+	// functions overloading are considerable
 
-	void newTable (string name, TimePeriod period);
-	//create a new timetable, save it with this name
+	bool newTable (string name, TimePeriod period);
+	// create a new timetable, save it with this name
+	// return false if the name is already exist
 	list<Template> addInTable (Template task);
-	//add tasks periodically to the table
-	//return a list of clashed tasks if any
+	// add tasks periodically to the table
+	// return a list of clashed tasks if any
 	void editInTable (/*argument to be specified*/);
 	void eraseInTable (/*argument to be specified*/);
-	//function overloading are considerable
-	//edit or erase all task periodically
+	// function overloading are considerable
+	// edit or erase all task periodically
 	void eraseTable (string name);
-	//erase the whole Table with the name specified
+	// erase the whole Table with the name specified
 	list<TimePeriod> eraseTable (TimePeriod period);
-	//return a list of periods of a similar period if cannot find the table with the time period
-	//return a clear list if be able to erase
+	// return a list of periods of a similar period if cannot find the table with the time period
+	// return a clear list if be able to erase
 
 public:
 	ToDoMngr ();
