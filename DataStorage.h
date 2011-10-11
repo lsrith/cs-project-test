7c6d13dacb1568f84636d56f9299686a89433b89
sinchien90 <sinchien90@gmail.com>
2011-10-11 19:24:02 +0000

    added new function prototypes (last 3 prototypes)

--- DataStorage.h
+++ DataStorage.h
@@ -1,7 +1,6 @@
 /*
-*Author: Chew
+* author Chew
 */
-
 #ifndef DATASTORAGE_H_
 #define DATASTORAGE_H_
 #include "TimePeriod.h"
@@ -13,35 +12,38 @@ using namespace std;
 class DataStorage {
 
 private: 
-        string _storageFile;
-        int _largestIndex;
-        
-        int reIndexingTemplates ();
-        // when to do it is based on your decision
-        // return the largest index
+	string _storageFile;
+	int _largestIndex;
+	
+	int reIndexingTemplates ();
+	// when to do it is based on your decision
+	// return the largest index
 public:
-        DataStorage (string storageFile);
-        void save (list<Task> toDoList);
-        // save the templates to the storageFile by overwriting any overlapping TimePeriod
-        // unless forceAdd == true for any particular template
-        // give an idex to each templates if it's not given
-        list<Task> load (TimePeriod period);
-        // return a list of templates within a time period input :) get it from our storageFile
-        void erase (list<int> templateIndex);
-        // erase any template with the specified index from the file
+	DataStorage (string storageFile);
+	void save (list<Task> toDoList);
+	// save the templates to the storageFile by overwriting any overlapping TimePeriod
+	// unless forceAdd == true for any particular template
+	// give an idex to each templates if it's not given
+	list<Task> load (TimePeriod period);
+	// return a list of templates within a time period input :) get it from our storageFile
+	void erase (list<int> templateIndex);
+	// erase any template with the specified index from the file
 
-        void save (string tableName, TimePeriod period, list<int> templateIndex);
-        // save the timetable with the name and period so that you can load back all tasks
-        list<Task> load (string tableName);
-        // return a list of templates of the timetable
-        list<string> load_table_name ();
-        // return a list of names of existing timetable
+	void save (string tableName, TimePeriod period, list<int> templateIndex);
+	// save the timetable with the name and period so that you can load back all tasks
+	list<Task> load (string tableName);
+	// return a list of templates of the timetable
+	list<string> load_table_name ();
+	// return a list of names of existing timetable
 
-        string search (string searchedWord, searchType type);
-        // search based on any specified word.. return a string to be displayed
-        // type to indicate exact match or similar match
-        string costomSearch (//arguments to be modify//);
-        // search based on some criteria
+	string search (string searchedWord, searchType type);
+	// search based on any specified word.. return a string to be displayed
+	// type to indicate exact match or similar match
+	string costomSearch (/*arguments to be modify*/);
+	// search based on some criteria
+	list<Task> sort_by_date(list<Task> toDoList);
+	void swap_element(list<Task> *ptr, list<Task> *ptr1);
+	void write_to_file(list<Task> todolist);
 
 };
 #endif
