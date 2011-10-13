b2e91d02a4845f0601448cecd51284f168f636c5
lsrith <lsrith@gmail.com>
2011-10-13 06:23:59 +0000

    modify save functions so that there is no need to assume that time or timePeriod of any existing tasks cannot be modified..

--- DataStroage.cpp
+++ DataStroage.cpp
@@ -133,6 +133,9 @@ void DataStorage::save (list<Task>* taskList)
 {
 	if (taskList->empty ()) return;
 
+  Task* taskPtr;
+  list<Task> timePeriodModifiedTasks;
+  list<int> timePeriodModifiedIndex;
 	list<Task>::iterator iter;
 	list<Time::date_t> dateList;	
 	for(iter = taskList->begin(); iter != taskList->end(); iter++)
@@ -151,7 +154,7 @@ void DataStorage::save (list<Task>* taskList)
 	updateDates (dateList);
 
 //timePeriod related change
-//for now, assume time and timePeriod wont be modified
+//for now, time and timePeriod can be modified
 	int dateIndex;
 	for(iter = taskList->begin(); iter != taskList->end(); iter++)
 	{	
@@ -175,60 +178,28 @@ void DataStorage::save (list<Task>* taskList)
 		}
 		else
 		{
-			tasks[iter->_index] = *iter;
-		}
+      //check if time or period is modified
+      taskPtr = &tasks[iter->_index];
+      if (taskPtr->get_time () == iter->get_time () && taskPtr->get_period ().get_start_time () == iter->get_period ().get_start_time ()
+        && taskPtr->get_period ().get_end_time () == iter->get_period ().get_end_time ()) {
+        *taskPtr = *iter;
+      } else {
+        iter->_index = 0;
+        timePeriodModifiedTasks.push_back (*iter);
+        timePeriodModifiedIndex.push_back (taskPtr->_index);
+      }
+  	}
 	}
+   
+  erase (timePeriodModifiedIndex);
+  save (&timePeriodModifiedTasks); 
 }
 
 void DataStorage::save(list<Task> taskList)
 {
-	if (taskList.empty ()) return;
-
-	list<Task>::iterator iter;
-	list<Time::date_t> dateList;	
-	for(iter = taskList.begin(); iter != taskList.end(); iter++)
-	{
-		if (iter->timeTask == true) {
-			dateList.push_back (iter->get_time ().get_date ());
-		} else {
-			Time time;
-			TimePeriod period = iter->get_period ();
-			for (time = period.get_start_time (); time.get_date () != period.get_end_time ().get_date (); time = time + Time::DAY) {
-				dateList.push_back (time.get_date ());
-			}
-			dateList.push_back (time.get_date ());
-		}
-	}
-	updateDates (dateList);
-
-//timePeriod related change
-//for now, assume time and timePeriod wont be modified
-	int dateIndex;
-	for(iter = taskList.begin(); iter != taskList.end(); iter++)
-	{	
-		if (iter->_index == 0)
-		{
-			iter->_index = _largestIndex;
-			_largestIndex++;
-			tasks.push_back (*iter);
-
-			if (iter->timeTask == true) {
-				dateIndex = getDateIndex (iter->get_time ().get_date ());
-				arrangedTask[dateIndex].push_back (iter->_index);
-			} else {
-				TimePeriod period = iter->get_period ();
-				int startIndex = getDateIndex (iter->get_period ().get_start_time ().get_date ());
-				int endIndex = getDateIndex (iter->get_period ().get_end_time ().get_date ());
-				for (dateIndex = startIndex; dateIndex <= endIndex; dateIndex++) {
-					arrangedTask[dateIndex].push_back (iter->_index);
-				}
-			}
-		}
-		else
-		{
-			tasks[iter->_index] = *iter;
-		}
-	}
+  list<Task>* taskListPtr;
+  taskListPtr = &taskList;
+  save (taskListPtr);
 }
 
 void DataStorage::updateDates (list<Time::date_t> dateList) 
