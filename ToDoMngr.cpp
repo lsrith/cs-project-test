7c087dd05baa36f7aab242e74695563bb73905b8
ris09livefan <ris09livefan@gmail.com>
2011-10-26 08:45:59 +0000

    the new view() codes...:)..sorry for the delay!!!!!

--- ToDoMngr.cpp
+++ ToDoMngr.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
 #include "ToDoMngr.h"
-#include "TimePeriod.h"
-#include "Task.h"
-#include "DataStorage.h"
 #include <list>
 #include <queue>
 #include <string>
@@ -10,7 +6,9 @@
 #include <fstream>
 #include <ctime>
 #include <iomanip>
+#include <iostream>
 using namespace std;
+
 
 ToDoMngr::ToDoMngr () {	
 }
@@ -19,87 +17,336 @@ list<Task> ToDoMngr::get_active_list () {
 	return _activeTaskList;
 }
 
- string ToDoMngr::view(Task& taskId){
-	    Time _time;
-		 std::ostringstream tsk;
+string ToDoMngr::view(Task taskId){
+     Time obj;
+                       
+						
+		int full_date,_date,temp_date;
+		temp_date=0;
+		string month,temp_month;
+		temp_month=" ";
+		int year;
+        std::ostringstream oss;
+       
+              // list<Task> _activeTasklist;
+               
+                
+                        if(taskId.get_time()==obj){
+                
+                    full_date=taskId.get_period().get_start_time().get_date();
+					_date=full_date/1000000;
+				     month=taskId.get_time().display_month((full_date % 1000000) / 10000);
+					year=full_date%10000;
+				
+								oss<<"\n";
+                                oss<<month<<" "<<year<<"  ";
+							    oss<<_date<<" "<<taskId.get_time().display_day(taskId.get_period().get_start_time().get_day())<<"\n";
+								oss<<"\n";
+							
+								oss<<taskId.get_period().get_start_time().string_clock()<<" "<<"-"<<" "<<taskId.get_period().get_end_time().string_clock();
+								oss<<" "<<taskId.note<<" "<<"at"<<" "<<taskId.venue;
+								
+							}
+							
+                        
+                        else
+                        {
+							full_date=taskId.get_time().get_date();
+					    _date=full_date/1000000;
+					     month=taskId.get_time().display_month((full_date % 1000000) / 10000);
+					     year=full_date%10000;
+				
+							
+								oss<<"\n";
+                                oss<<month<<" "<<year<<"  ";
+							    oss<<_date<<" "<<taskId.get_time().display_day(taskId.get_time().get_day())<<"\n";
+								oss<<"\n";
+								
+								oss<<taskId.get_time().string_clock();
+								oss<<" "<<taskId.note<<" "<<"at"<<" "<<taskId.venue;
+								
+                               /* oss<<l->get_period().string_time_period()<<"\n"<<index<<"."
+                                <<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;*/
+							}
+                        
 
-		if(taskId.get_time()==_time)
-		{
-			tsk<<taskId.get_period().string_time_period()<<" "<<" "<<taskId.note<<" "<<taskId.venue;
-		}
-		else
-		{
-    
-		tsk<<taskId.get_time().string_date()<<" "<<taskId.get_time().string_clock()<<" "<<taskId.note<<" "<<taskId.venue;
-		}
-        return tsk.str();
+                         return oss.str();
+	
+	
+	/*list<Task> _tempList;
+	 _tempList.push_back(taskId);
+	 return view(_tempList);*/
+	
+	/* Time _time;
+                 std::ostringstream tsk;
+				 int full_date,_date,temp_date;
+		temp_date=0;
+		string month,temp_month;
+		temp_month=" ";
+		int year;
+
+                if(taskId.get_time()==_time)
+                {
+					     full_date=taskId.get_period().get_start_time().get_date();
+					_date=full_date/1000000;
+					
+					
+					month=taskId.get_time().display_month((full_date % 1000000) / 10000);
+					year=full_date%10000;
+				
+							if(temp_month==month){
+							}
+							else{
+								tsk<<"\n";
+                             tsk<<month<<" "<<year<<"\n";
+							tsk<<"--------"<<"\n";
+							temp_month=month;
+							temp_date=0;
+						}
+
+									if(temp_date==_date){
+									
+									//oss<<index<<"."<<" ";
+								tsk<<taskId.get_period().get_start_time().string_clock()<<" "<<"-"<<" "<<taskId.get_period().get_end_time().string_clock();
+								tsk<<" "<<taskId.note<<" "<<"at"<<" "<<taskId.venue<<endl;
+									}
+									else
+									{
+								tsk<<"\n";		
+								tsk<<_date<<" "<<l->get_time().display_day(l->get_period().get_start_time().get_day())<<"\n";
+								tsk<<"\n";
+								//oss<<index<<"."<<" ";
+								tsk<<taskId.get_period().get_start_time().string_clock()<<" "<<"-"<<" "<<taskId.get_period().get_end_time().string_clock();
+							    tsk<<" "<<taskId.note<<" "<<"at"<<" "<<taskId.venue<<endl;
+								temp_date=_date;
+                            
+							}
+							
+                        //tsk<<taskId.get_period().string_time_period()<<" "<<" "<<taskId.note<<" "<<taskId.venue;
+                }
+                else
+                {
+                      full_date=taskId.get_time().get_date();
+					_date=full_date/1000000;
+					
+					
+					month=taskId.get_time().display_month((full_date % 1000000) / 10000);
+					year=full_date%10000;
+				
+							if(temp_month==month){
+							}
+							else{
+								tsk<<"\n";
+                            	tsk<<month<<" "<<year<<"\n";
+								tsk<<"--------"<<"\n";
+							temp_month=month;
+							temp_date=0;
+						}
+
+									if(temp_date==_date){
+									
+									//oss<<index<<"."<<" ";
+										tsk<<taskId.get_time().string_clock();
+								    	tsk<<" "<<taskId.note<<" "<<"at"<<" "<<taskId.venue<<endl;
+									}
+									else
+									{
+									tsk<<"\n";		
+									tsk<<_date<<" "<<taskId.get_time().display_day(taskId.get_time().get_day())<<"\n";
+									tsk<<"\n";
+								//oss<<index<<"."<<" ";
+									tsk<<taskId.get_time().string_clock();
+									tsk<<" "<<taskId.note<<" "<<"at"<<" "<<taskId.venue<<endl;
+								temp_date=_date;
+                            
+							}
+							
+                //tsk<<taskId.get_time().string_date()<<" "<<taskId.get_time().string_clock()<<" "<<taskId.note<<" "<<taskId.venue;
+                }
+        return tsk.str();*/
 }
 
 string ToDoMngr:: view(list<Task> tasklist){
         Time obj;
+		int full_date,_date,temp_date,temp_year;
+		temp_date=0;
+		string month,temp_month;
+		temp_month=" ";
+		temp_year=0;
+		int year;
+	
         std::ostringstream oss;
         list<Task> taskl=tasklist;
     list<Task>::iterator l;
-	int index=1;
-	if(tasklist.empty())
-		oss<<" ";
-	else{
+        int index=1;
+        if(tasklist.empty())
+                oss<<" ";
+        else{
 
         for(l =taskl.begin (); l !=taskl.end (); l++)
                 {   
+					
+
+
                         if(l->get_time()==obj){
-                                oss<<l->get_period().string_time_period()<<"\n"<<index<<"."
-                                <<" "<<l->note<<" "<<l->venue<<endl;
-                        }
+							full_date=l->get_period().get_start_time().get_date();
+					_date=full_date/1000000;
+					
+					
+					month=l->get_time().display_month((full_date % 1000000) / 10000);
+					year=full_date%10000;
+				
+							if(temp_month==month){
+							}
+							else{
+								oss<<"\n";
+                             oss<<month<<" ";
+							 
+							 if(temp_year==year)
+							oss<<temp_year<<"\n";
+							 else
+						    oss<<year<<"\n";
+							
+							 oss<<"--------"<<"\n";
+							temp_month=month;
+							temp_year=year;
+							temp_date=0;
+						}
+
+									if(temp_date==_date){
+									
+									oss<<index<<"."<<" ";
+								oss<<l->get_period().get_start_time().string_clock()<<" "<<"-"<<" "<<l->get_period().get_end_time().string_clock();
+								oss<<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;
+									}
+									else
+									{
+								oss<<"\n";		
+								oss<<_date<<" "<<l->get_time().display_day(l->get_period().get_start_time().get_day())<<"\n";
+								oss<<"\n";
+								oss<<index<<"."<<" ";
+								oss<<l->get_period().get_start_time().string_clock()<<" "<<"-"<<" "<<l->get_period().get_end_time().string_clock();
+								oss<<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;
+								temp_date=_date;
+                               /* oss<<l->get_period().string_time_period()<<"\n"<<index<<"."
+                                <<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;*/
+							}
+							
+								
+                        
+						}
                         else
                         {
-							oss<<l->get_time().string_date() << " at " <<l->get_time().string_clock()<<"\n"<<index<<"."
-                                <<" "<<l->note<<" "<<l->venue<<endl;
+							full_date=l->get_time().get_date();
+					_date=full_date/1000000;
+					
+					
+					month=l->get_time().display_month((full_date % 1000000) / 10000);
+					year=full_date%10000;
+				
+							if(temp_month==month){
+							}
+							else{
+								oss<<"\n";
+                             oss<<month<<" "<<year<<"\n";
+							oss<<"--------"<<"\n";
+							temp_month=month;
+							temp_date=0;
+						}
+
+									if(temp_date==_date){
+									
+									oss<<index<<"."<<" ";
+									oss<<l->get_time().string_clock();
+								    oss<<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;
+									}
+									else
+									{
+								oss<<"\n";		
+								oss<<_date<<" "<<l->get_time().display_day(l->get_time().get_day())<<"\n";
+								oss<<"\n";
+								oss<<index<<"."<<" ";
+								oss<<l->get_time().string_clock();
+								oss<<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;
+								temp_date=_date;
+                               /* oss<<l->get_period().string_time_period()<<"\n"<<index<<"."
+                                <<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;*/
+							}
+							
+                                                       /* oss<<l->get_time().string_date() << " at " <<l->get_time().string_clock()<<"\n"<<index<<"."
+                                <<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;*/
 
                         }
-						index++;
+                                                index++;
         }
-	}
+        }
    return oss.str();
-	
+        
 }
 
 string ToDoMngr::view (int taskId){
             Time obj;
-			ToDoMngr todo;
-        std::ostringstream tsk;
+                        ToDoMngr todo;
+						
+		int full_date,_date,temp_date;
+		temp_date=0;
+		string month,temp_month;
+		temp_month=" ";
+		int year;
+        std::ostringstream oss;
         int i=1;
               // list<Task> _activeTasklist;
-	       list<Task>::iterator _taskpointer;
-		   _taskpointer=_activeTaskList.begin();
+               list<Task>::iterator _taskpointer;
+                   _taskpointer=_activeTaskList.begin();
 
-				/*for(_taskpointer=todo.get_active_list().begin();_taskpointer!=todo.get_active_list().end();_taskpointer++)
-				{
-					if(i<taskId){
-						i++;
-						continue;
-					}
-					else
-						break;*/
-					
-                
-                for(int i=1;i<taskId;i++){
-					if(_taskpointer!=_activeTaskList.end())
-				     _taskpointer++;
-                }
+                                  for(int i=1;i<taskId;i++){
+                                        if(_taskpointer!=_activeTaskList.end())
+                                     _taskpointer++;
+                                  }
                 
                         if(_taskpointer->get_time()==obj){
                 
-        tsk<<_taskpointer->get_period().string_time_period ()<<
-                                " "<<"\n"<<_taskpointer->note<<" "<<"Venue:"<<" "<<_taskpointer->venue<<endl;
-                        }
+                    full_date=_taskpointer->get_period().get_start_time().get_date();
+					_date=full_date/1000000;
+				     month=_taskpointer->get_time().display_month((full_date % 1000000) / 10000);
+					year=full_date%10000;
+				
+								oss<<"\n";
+                                oss<<month<<" "<<year<<"\n";
+							    oss<<"--------"<<"\n";
+						        oss<<_date<<" "<<_taskpointer->get_time().display_day(_taskpointer->get_period().get_start_time().get_day())<<"\n";
+								oss<<"\n";
+								oss<<taskId<<"."<<" ";
+								oss<<_taskpointer->get_period().get_start_time().string_clock()<<" "<<"-"<<" "<<_taskpointer->get_period().get_end_time().string_clock();
+								oss<<" "<<_taskpointer->note<<" "<<"at"<<" "<<_taskpointer->venue<<endl;
+								
+							}
+							
+                        
                         else
                         {
-							tsk<<_taskpointer->get_time().string_date()<<_taskpointer->get_time().string_clock()
-                                <<" "<<"\n"<<_taskpointer->note<<" "<<"Venue:"<<" "<<_taskpointer->venue<<endl;
-                        }
+							full_date=_taskpointer->get_time().get_date();
+					    _date=full_date/1000000;
+					     month=_taskpointer->get_time().display_month((full_date % 1000000) / 10000);
+					     year=full_date%10000;
+				
+							
+								oss<<"\n";
+                                oss<<month<<" "<<year<<"\n";
+							    oss<<"--------"<<"\n";
+						        oss<<"\n";		
+								oss<<_date<<" "<<_taskpointer->get_time().display_day(_taskpointer->get_time().get_day())<<"\n";
+								oss<<"\n";
+								oss<<taskId<<"."<<" ";
+								oss<<_taskpointer->get_time().string_clock();
+								oss<<" "<<_taskpointer->note<<" "<<"at"<<" "<<_taskpointer->venue<<endl;
+								temp_date=_date;
+                               /* oss<<l->get_period().string_time_period()<<"\n"<<index<<"."
+                                <<" "<<l->note<<" "<<"at"<<" "<<l->venue<<endl;*/
+							}
+                        
 
-                         return tsk.str();
+                         return oss.str();
                 
         }   // return a string of the view of the specific task
 
@@ -133,15 +380,15 @@ string ToDoMngr::view(view_t viewType, Time time){
                 TimePeriod period(start,end);
                 _activeTaskList= _dataStorage.load(period);
                 
-                return DATE+" "+":"+"\n"+view(_activeTaskList);
+                return view(_activeTaskList);
         }
                  
         else if(viewType=WEEKLY){
       Task _day;
          int day;
-	for(int i=0;i<7;i++){
-		day=_day.get_time().get_day();
-		if(_day.get_time().display_day(day)==days[i]){
+        for(int i=0;i<7;i++){
+                day=_day.get_time().get_day();
+                if(_day.get_time().display_day(day)==days[i]){
                          _index=i;
                         break;
                 }
@@ -214,7 +461,7 @@ string ToDoMngr::help (string command){
         ifstream myhelpfile;
         myhelpfile.open("HELP.txt");
         string HELP_LINE;
-		ostringstream str;
+                ostringstream str;
 
         if(command.empty ()){
                 while(getline(myhelpfile, HELP_LINE))
@@ -223,7 +470,7 @@ string ToDoMngr::help (string command){
                 }
         }
 
-		return str.str ();
+                return str.str ();
 }
 
 string ToDoMngr::reminder(){
@@ -231,7 +478,7 @@ string ToDoMngr::reminder(){
         string Date;
         string Month;
         string FINAL_FORMAT;
-        std::ostringstream oss;
+        std::ostringstream oss,_format;
 
         time_t t = time(0);   //get the current time
         struct tm * now = localtime( & t );
@@ -240,12 +487,16 @@ string ToDoMngr::reminder(){
         Date=oss.str();        //Stores the current computer date
 
         Time time(atoi(Date.c_str()),t);
-        return "Your Tasks for the day are as follows"+view(DAILY,time);   //pass to view() to search for the tasks on that particular date and display it...year is not taken into account at present.
-
-
-
-
+		string newline="\n";
+	   
+		_format<<"\n"<<setw(47)<<setfill(' ')<<"AGENDA  FOR  TODAY"<<"\n";
+	   _format<<"\n";
+	   _format<<"Your Tasks for the day are as follows -->"<<"\n";
+	   _format<<view(DAILY,time);
+        return _format.str();//+view(DAILY,time);   //pass to view() to search for the tasks on that particular date and display it...year is not taken into account at present.
 }
+
+
 
 list<Task> ToDoMngr::add(Task task, bool forceAdd)                                                                      
 {
