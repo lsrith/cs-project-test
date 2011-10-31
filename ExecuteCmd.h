c931609d8b1b91211b8456e18f1dbb5871d250a2
lsrith <lsrith@gmail.com>
2011-10-31 20:16:56 +0800

    Adding AccCtrl class Attempt multiple inheritance

--- ExecuteCmd.h
+++ ExecuteCmd.h
@@ -1,23 +1,17 @@
 #ifndef EXECUTECMD_H
 #define EXECUTECMD_H
 #include "VldCmdCtrl.h"
+#include "CmdTranslator.h"
 #include "ToDoMngr.h"
 using namespace std;
 
 template <typename data_t>
 void append_back (list<data_t>* main, list<data_t> sub);
 
-class ExecuteCmd: public VldCmdCtrl {
-protected:
-	typedef unsigned short int indx_t;
-	enum input_t {CMD, DATA, NONE};
-
+class ExecuteCmd {
 public:
 	ExecuteCmd ();
 	~ExecuteCmd ();
-	//Command will load CmdList from built-in file
-	//true if the user wants the '.' command
-	//false if the user wants the literal command
 
 	void updateInput (string& input);
 	string get_input ();
@@ -26,44 +20,13 @@ public:
 	virtual bool execute ();
 	string result ();
 
-	static string BREAK;
-	static string MORE;
-
 protected:
 	string _input;
 	string _result;
-	queue<command>* _cmdInput;
-	queue<string>* _dataInput;
-	queue<input_t>* _sequence;
-	list<string>* _splitedInput;
-	input_t _flagError;
 	ToDoMngr* _toDoMngr;
-
-	void splitInput ();
-	command translateCmd (string);
-	//return an fresh command and flag command error if command is not valid
-
-	int get_int ();
-	Task get_task ();
-	TimePeriod get_period ();
-	Time get_time ();
-	Time::date_t get_date ();
-	Time::clk_t get_clock ();
-	bool notMorning (string);
-	bool notMorning ();
-	void get_date (int&, int&, int&);
-	void convertToInt (string, int&, int&);
-	int convertToInt (string);	// return -1 if the whole string is not an integer
-	string mergeStringInput ();
-	string mergeSimStringInput ();
-
-	void push (command);
-	void push (string);
-	void clear ();
-	bool pop ();
 };
 
-class Add: public ExecuteCmd {
+class Add: public CmdTrans, public ExecuteCmd {
 public:
 	Add (vector<cmd_pair>, ToDoMngr*);
 	~Add ();
@@ -76,7 +39,7 @@ private:
 };
 
 //think of wrong id??
-class Edit: public ExecuteCmd {
+class Edit: public CmdTrans, public ExecuteCmd {
 public:
 	Edit (vector<cmd_pair>, ToDoMngr*);
 	~Edit ();
@@ -87,7 +50,7 @@ private:
 	void insertBreakPoint ();
 };
 
-class View: public ExecuteCmd {
+class View: public CmdTrans, public ExecuteCmd {
 public:
 	View (vector<cmd_pair>, ToDoMngr*);
 	~View ();
@@ -113,7 +76,7 @@ private:
 	bool _activeListAccessible;
 };
 
-class Delete: public ExecuteCmd {
+class Delete: public CmdTrans, public ExecuteCmd {
 public:
 	Delete (vector<cmd_pair>, ToDoMngr*);
 	~Delete ();
@@ -121,5 +84,12 @@ public:
 
 private:
 	static string MSG_DELETED;
+};
+
+class AccCtrl: public ExecuteCmd {
+public:
+	AccCtrl (ToDoMngr*);
+	~AccCtrl ();
+	bool execute ();
 };
 #endif
