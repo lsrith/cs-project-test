f1ed6bbef8666186b42d20145cd692f5429c3219
lsrith <lsrith@gmail.com>
2011-11-09 02:07:33 +0800

    Merge with Ben

--- ToDoMngr.h
+++ ToDoMngr.h
@@ -39,7 +39,7 @@ public:
 		Task* _eTask;
 		Task _task, _updatedTask;
 		int _taskId;
-		list<Task*> _deletePeriodList;
+		Task* _ptr;
 		list<int> _index;
 		TimePeriod _period;
 		string _tableName;
